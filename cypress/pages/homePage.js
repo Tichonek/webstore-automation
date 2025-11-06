7c7420216909596380f3a8bf5fab4ae136aea3ad
Tichonek <meksykanin.32@gmail.com>
2025-11-06 20:50:16 +0100

    add homePage and LoginPage. Modify example.cy.js test

---
+++ cypress/pages/homePage.js
@@ -0,0 +1,9 @@
+export class HomePage {
+    elements = {
+        signInLink: () => cy.get('a[title="Log in to your customer account"] > span')
+    }
+
+    goToLoginPage() {
+        this.elements.signInLink().click();
+    }
+}
---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,29 @@
+export class LoginPage {
+    elements = {
+        emailInput: () => cy.get('#field-email'),
+        passInput: () => cy.get('#field-password'),
+        forgotPassBtn: () => cy.get('div[class="forgot-password"] > a'),
+        signInBtn: () => cy.get('#submit-login'),
+        noAccountBtn: () => cy.get('div[class="no-account"] > a')
+    }
+
+    typeEmail(email) {
+        return this.elements.emailInput().clear().type(email);
+    }
+
+    typePass(pass) {
+        return this.elements.passInput().clear().type(pass);
+    }
+
+    clickSignIn() {
+        this.elements.signInBtn().click();
+    }
+
+    clickForgotPass() {
+        this.elements.forgotPassBtn().click();
+    }
+
+    clickNoAccountBtn() {
+        this.elements.noAccountBtn().click();
+    }
+}
