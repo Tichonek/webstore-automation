ffe67dc4bd6cb2ecbcdeff0c6b4888c41bdd86df
Tichonek <meksykanin.32@gmail.com>
2025-11-08 15:08:14 +0100

    add data generator

---
+++ cypress/scripts/config.py
@@ -0,0 +1,13 @@
+DOMAINS = ['@example.com', '@test.pl', '@wp.pl', '@mail.net', '@gmail.com']
+
+PASSWORD_CONFIG = {
+    'min_length': 8,
+    'max_length': 12,
+    'include_digits': True,
+    'include_specials': True
+}
+
+YEAR_CONFIG = {
+    'min_year': 1960,
+    'max_year': 2010
+}
---
+++ cypress/scripts/data_generators.py
@@ -0,0 +1,107 @@
+from config import DOMAINS, PASSWORD_CONFIG, YEAR_CONFIG
+from faker import Faker
+from unidecode import unidecode
+import random
+
+faker = Faker('pl-PL')
+
+def generateFirstName():
+    firstName = faker.first_name()
+    return firstName
+
+def generateLastName():
+    lastName = faker.last_name()
+    return lastName
+
+def generateEmail(firstName, lastName):
+    domain = random.choice(DOMAINS)
+    firstName = unidecode(firstName.lower())
+    lastName = unidecode(lastName.lower())
+    email = f'{firstName}.{lastName}{domain}'
+    return email
+
+def generatePassword():
+    min_length = PASSWORD_CONFIG['min_length']
+    max_length = PASSWORD_CONFIG['max_length']
+    include_digits = PASSWORD_CONFIG['include_digits']
+    include_specials = PASSWORD_CONFIG['include_specials']
+
+
+    chars = []
+    chars.extend(char for char in 'abcdefghijklmnopqrstuvwxyz')
+    chars.extend(char for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    
+    if include_digits:
+        chars += '0123456789'
+    
+    if include_specials:
+        chars += '!@#$%^&*()'
+    
+    passwordLen = random.randint(min_length, max_length)
+    passwordChars = []
+    print(passwordLen)
+
+    for char in range(passwordLen):
+        char = random.choice(chars)
+        passwordChars.append(char)
+        print(char)
+    
+    password = ''.join(passwordChars)
+
+    return password
+
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    
+    if year % 100 == 0:
+        return False
+    
+    if year % 4 == 0:
+        return True
+
+def generateBirthDate():
+    min_year = YEAR_CONFIG['min_year']
+    max_year = YEAR_CONFIG['max_year']
+
+    year = random.randint(min_year, max_year)
+
+    month = random.randint(1,12)
+
+    
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        maxDay = random.randint(1, 31)
+    elif month in (4, 6, 9, 11):
+        maxDay = random.randint(1, 30)
+    elif month == 2:
+        if isLeapYear(year):
+            maxDay = 29
+        else:
+            maxDay = 28
+
+    day = random.randint(1,maxDay)
+    
+    birthDate = f'{month:02d}/{day:02d}/{year}'
+    return birthDate
+
+def generatePerson():
+    firstName = generateFirstName()
+    lastName = generateLastName()
+    person = {
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': generateEmail(firstName, lastName),
+        'password': generatePassword(),
+        'birthDate': generateBirthDate()
+    }
+
+firstName = generateFirstName()
+print(firstName)
+lastName = generateLastName()
+print(lastName)
+email = generateEmail(firstName, lastName)
+print(email)
+password = generatePassword()
+print(password)
+birthDate = generateBirthDate()
+print(birthDate)
